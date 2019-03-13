10215faebdb415bccb60aabcc4dc12e958065ff2
zhenghaven <hazheng@mix.wvu.edu>
2019-03-12 17:06:56 -0700

    Added sample code for proc msg from store.

--- sources/Common_Enc/Dht/Sgx/DecentDht.cpp
+++ sources/Common_Enc/Dht/Sgx/DecentDht.cpp
@@ -6,6 +6,7 @@
 
 #include "../../../Common/Dht/AppNames.h"
 #include "../DhtStatesSingleton.h"
+#include "../EnclaveStore.h"
 
 using namespace Decent;
 using namespace Decent::Dht;
@@ -27,6 +28,22 @@ extern "C" int ecall_decent_dht_proc_msg_from_dht(void* connection)
 	Decent::Net::TlsCommLayer tls(connection, tlsCfg, true);
 
 	ProcessDhtQueries(connection, tls);
+
+	return false;
+}
+
+extern "C" int ecall_decent_dht_proc_msg_from_store(void* connection)
+{
+	if (!gs_state.GetDhtNode())
+	{
+		LOGW("Local DHT Node had not been initialized yet!");
+		return false;
+	}
+
+	std::shared_ptr<Decent::Ra::TlsConfig> tlsCfg = std::make_shared<Decent::Ra::TlsConfig>(AppNames::sk_decentDHT, gs_state, true);
+	Decent::Net::TlsCommLayer tls(connection, tlsCfg, true);
+	
+	//gs_state.GetDhtStore().RecvMigratingData()
 
 	return false;
 }
